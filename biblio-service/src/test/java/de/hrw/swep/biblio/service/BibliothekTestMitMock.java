309f572f6cb50ea1c209f897252ab1de421a0a5c
oa-141398 <oa-141398@AP086121.hrw.hs-weingarten.de>
2016-01-28 20:38:57 +0100

    commit serviceschicht

--- biblio-service/src/test/java/de/hrw/swep/biblio/service/BibliothekTestMitMock.java
+++ biblio-service/src/test/java/de/hrw/swep/biblio/service/BibliothekTestMitMock.java
@@ -2,10 +2,17 @@ package de.hrw.swep.biblio.service;
 
 import static org.junit.Assert.fail;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
+
+import de.hrw.swep.biblio.persistence.DBInterface;
+import de.hrw.swep.biblio.persistence.dto.BenutzerDTO;
+import static org.mockito.Mockito.*;
 
 /**
  * Testet die Bibliotheks-Klasse mit einem Mock-Objekt
+ * 
  * @author Sandra
  *
  */
@@ -14,6 +21,20 @@ public class BibliothekTestMitMock {
 	/**
 	 * Testet, ob ein Buch mit gegebenem Titel gefunden wird.
 	 */
+
+	Bibliothek bib;
+	private DBInterface db;
+
+	@Before
+	public void setUp() {
+		db = Mockito.mock(DBInterface.class);
+		Mockito.when(db.getBenutzerById(1)).thenReturn(
+				new BenutzerDTO(1, "erceng", "Gesperrt"));
+
+		// db.When.getBuchByAutor("Kaka").thenReturn
+
+	}
+
 	@Test
 	public void testSucheBuchNachTitel() {
 		fail("Not yet implemented");
